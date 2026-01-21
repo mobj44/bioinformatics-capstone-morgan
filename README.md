253feb4eb986efe1236f75a80bec2b0123b6cea2
Morgan Johnston <morganbjohnston@gmail.com>
2026-01-21 14:44:18 -0700

    readme and validate md5sum

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # bioinformatics-capstone-morgan
+
+## Environment Setup  
+### Conda 
+conda env create -f environment.yml
+conda activate illumina-qc
+fastp --version
+fastqc --version
+
+
+
--- scripts/validate_md5sum.sh
+++ scripts/validate_md5sum.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/usr/bin/env bash
-
 # loop over the gz files
 for i in ./data/illumina_data_reads/*.gz; do
   # get the sample name , e.g. AC2 from AC2_S144_R1_001.fastq.gz
@@ -9,15 +7,15 @@ for i in ./data/illumina_data_reads/*.gz; do
 
   # determine if file is R1 or R2
   if echo "$i" | grep -q "_R1_"; then
-    # expected R1 md5 is column 6 
-    expected="$(cat ./data/illumina_data_reads/DNA_sequencing_stats.csv | grep "^$sample" | cut -d , -f6)"
+    # expected R1 md5 is column 6
+    expected="$(cat ./data/illumina_data_reads/DNA_sequencing_stats.csv | grep "^$sample" | cut -d , -f6 | tr -d '\r[:space:]"')"
   else
-    # expected R2 md5 is column 7 
-    expected="$(cat ./data/illumina_data_reads/DNA_sequencing_stats.csv | grep "^$sample" | cut -d , -f7)"
+    # expected R2 md5 is column 7
+    expected="$(cat ./data/illumina_data_reads/DNA_sequencing_stats.csv | grep "^$sample" | cut -d , -f7 | tr -d '\r[:space:]"')"
   fi
 
   # actual md5 from the file
-  actual="$(md5sum "$i" | cut -d ' ' -f1)"
+  actual="$(md5sum "$i" | cut -d ' ' -f1 | tr -d '\r[:space:]')"
 
   # compare and print
   if [ "$actual" = "$expected" ]; then
