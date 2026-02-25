39b2e8c4ae9ab645fcfb21e216072bd3debeea25
Morgan Johnston <morganbjohnston@gmail.com>
2026-02-25 13:17:45 -0700

    feat: quast script

--- scripts/run_quast.sh
+++ scripts/run_quast.sh
@@ -4,10 +4,12 @@ set -e
 INPUT=/input
 OUTPUT=/output
 
+
+
 for FILE in $INPUT/*.fna; do
   [ -e "$FILE" ] || { echo "No .fna files found"; exit 1; }
 
   BASE="$(basename "$FILE" .fna)"   
 
-  echo "todo: quast"
+  quast.py $FILE -o "${OUTPUT}/${BASE}"
 done
